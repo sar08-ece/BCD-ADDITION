cd818b0afdcf4726ca11da5c7be9f6330b39112b
SARAVANAN R S V <saravananedu08@gmail.com>
2025-06-13 20:18:27 +0530

    TESTING COMMITS

--- bcdadder_1.v
+++ bcdadder_1.v
@@ -1,14 +1,14 @@
-module bcdadder_1(a,b,carry_in,sum,carry);
+module bcdadder_1(a,b,carry_ins,sum,carry);
     input[3:0]a,b;
-    input carry_in;
+    input carry_ins;
     output[3:0]sum;
     output carry;
     reg[4:0]sum_temp;
     reg[3:0]sum;
     reg carry;
-    always @(a,b,carry_in)
+    always @(a,b,carry_ins)
     begin
-        sum_temp=a+b+carry_in;
+        sum_temp=a+b+carry_ins;
         if(sum_temp>9)
         begin
             sum_temp=sum_temp+6;
